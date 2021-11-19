0f91675a3a12cd93b7bedf68c2b9fbbbd2e6364e
naufaldi <naufaldi.rafif@gmail.com>
2021-11-19 08:30:32 +0700

    fix whatsapp list message

--- components/ListMessage/ListMessageButton.tsx
+++ components/ListMessage/ListMessageButton.tsx
@@ -1,16 +1,22 @@
 import React, { FC, Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { interactiveButtonsAtom, titleInteractiveButtonsAtom } from '../../Recoil/interactiveButton';
+import { overlayAtom } from '../../Recoil/overlay';
 
 const ListMessageButton: FC = () => {
   const [isShowMessage, setShowMessage] = useState(false);
+  const [isOpened, setOpened] = useRecoilState(overlayAtom);
   const message = useRecoilValue(interactiveButtonsAtom);
   const titleMessage = useRecoilValue(titleInteractiveButtonsAtom);
+  const handleClick = () => {
+    setShowMessage(!isShowMessage);
+    setOpened(!isOpened);
+  };
   console.log('List Message', message);
   return (
     <>
-      <div className='w-full min-h-[20px] bg-white rounded-lg shadow z-10 px-2 py-2 font-sans text-center text-[#0099CB] font-normal col-span-full flex items-center justify-center' onClick={() => setShowMessage(!isShowMessage)}>
+      <div className='w-full min-h-[20px] bg-white rounded-lg shadow z-10 px-2 py-2 font-sans text-center text-[#0099CB] font-normal col-span-full flex items-center justify-center' onClick={handleClick}>
         <svg width='16' height='14' viewBox='0 0 16 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
           <path d='M14.9968 2.67981H5.81604C5.35232 2.67981 4.9776 2.14974 4.9776 1.50189C4.9776 0.854037 5.35232 0.323975 5.81604 0.323975H14.9968C15.4605 0.323975 15.8352 0.854037 15.8352 1.50189C15.8352 2.14974 15.4605 2.67981 14.9968 2.67981Z' fill='#0099CB' />
           <path d='M1.50184 3.00369C2.33129 3.00369 3.00369 2.33129 3.00369 1.50184C3.00369 0.672398 2.33129 0 1.50184 0C0.672398 0 0 0.672398 0 1.50184C0 2.33129 0.672398 3.00369 1.50184 3.00369Z' fill='#0099CB' />
@@ -21,13 +27,13 @@ const ListMessageButton: FC = () => {
         </svg>
         <p className='text-sm ml-2'> List Message</p>
       </div>
-      <div className='bg-black bg-opacity-20 top-0 left-0 w absolute'></div>
-      <Transition appear show={isShowMessage} className='absolute top-[30%] left-0 right-0 '>
+      <Transition appear show={isShowMessage && isOpened} className='bg-black bg-opacity-40 top-0 left-0 right-0 bottom-0 absolute z-10'></Transition>
+      <Transition appear show={isShowMessage && isOpened} className='absolute top-[64%] left-0 right-0 '>
         <Transition.Child enter='ease-out duration-300' enterFrom='opacity-0 scale-95' enterTo='opacity-100 scale-100' leave='ease-in duration-200' leaveFrom='opacity-100 scale-100' leaveTo='opacity-0 scale-95' as='div' className='bg-white rounded-tr-2xl rounded-tl-2xl w-full absolute max-h-[375px] left-0 right-0 z-20 px-[18px] py-[23px]'>
           <div className='flex flex-col h-full'>
             <div className='flex flex-col'>
               <div className='flex items-center justify-center relative'>
-                <button className='absolute top-0 left-0 h-4 w-6 flex items-center justify-center' onClick={() => setShowMessage(!isShowMessage)}>
+                <button className='absolute top-0 left-0 h-4 w-6 flex items-center justify-center' onClick={handleClick}>
                   <svg className=' h-4 w-4' viewBox='0 0 15 15' fill='none' xmlns='http://www.w3.org/2000/svg'>
                     <path d='M8.87422 7.50011L14.7149 1.65911C15.095 1.27918 15.095 0.664882 14.7149 0.28495C14.335 -0.0949832 13.7207 -0.0949832 13.3408 0.28495L7.49991 6.12595L1.65921 0.28495C1.27911 -0.0949832 0.665002 -0.0949832 0.285077 0.28495C-0.0950257 0.664882 -0.0950257 1.27918 0.285077 1.65911L6.12578 7.50011L0.285077 13.3411C-0.0950257 13.721 -0.0950257 14.3353 0.285077 14.7153C0.474417 14.9048 0.72337 15 0.972145 15C1.22092 15 1.46969 14.9048 1.65921 14.7153L7.49991 8.87428L13.3408 14.7153C13.5303 14.9048 13.7791 15 14.0279 15C14.2766 15 14.5254 14.9048 14.7149 14.7153C15.095 14.3353 15.095 13.721 14.7149 13.3411L8.87422 7.50011Z' fill='#7D8489' />
                   </svg>
--- components/LogiclessComponents.tsx
+++ components/LogiclessComponents.tsx
@@ -1,5 +1,8 @@
+import { Transition } from '@headlessui/react';
 import { FC } from 'react';
+import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
+import { overlayAtom } from '../Recoil/overlay';
 import HeaderWhatsApp from './WhatsAppPreview/HeaderWhatsApp';
 
 export const SectionHeading: FC<{ title: string } & React.HTMLAttributes<HTMLHeadingElement>> = ({ title, className, ...props }) => (
@@ -33,28 +36,32 @@ const PreviewComponentContainer = styled.div`
 `;
 
 export const PreviewComponent: FC = ({ children }) => {
+  const isOpened = useRecoilValue(overlayAtom);
   return (
     <PreviewComponentContainer className='relative flex-auto border-solid border-1 shadow-sm rounded-sm bg-[#e5ddd5] '>
       <div className='flex flex-col overflow-y-auto h-full'>
-        <div className='w-full flex justify-end items-center bg-whatsapp-500 py-1 px-2 space-x-1'>
-          <svg width='20' height='16' viewBox='0 0 20 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path fillRule='evenodd' clipRule='evenodd' d='M0.977371 4.01593C3.48822 2.12256 6.61301 1 10 1C13.387 1 16.5118 2.12256 19.0226 4.01593L10 15L0.977371 4.01593Z' fill='white' />
-          </svg>
+        <div className='flex flex-col relative'>
+          <Transition appear show={isOpened} className='bg-black bg-opacity-40 top-0 left-0 right-0 bottom-0 absolute z-10'></Transition>
+          <div className='w-full flex justify-end items-center bg-whatsapp-500 py-1 px-2 space-x-1'>
+            <svg width='20' height='16' viewBox='0 0 20 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path fillRule='evenodd' clipRule='evenodd' d='M0.977371 4.01593C3.48822 2.12256 6.61301 1 10 1C13.387 1 16.5118 2.12256 19.0226 4.01593L10 15L0.977371 4.01593Z' fill='white' />
+            </svg>
 
-          <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path fillRule='evenodd' clipRule='evenodd' d='M0 15H14V1L0 15Z' fill='white' />
-          </svg>
+            <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path fillRule='evenodd' clipRule='evenodd' d='M0 15H14V1L0 15Z' fill='white' />
+            </svg>
 
-          <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <path fillRule='evenodd' clipRule='evenodd' d='M9 1.875V1L6 1V1.875H3V15H12V1.875H9Z' fill='white' />
-          </svg>
-          <p className='text-sm text-white font-semibold'>12:30</p>
-        </div>
-        <div className='w-full flex items-center bg-whatsapp-600 pt-2 pb-3 px-2'>
-          <HeaderWhatsApp />
-          <svg width='12' height='24' viewBox='0 0 12 24' fill='none' className='ml-auto mr-2' xmlns='http://www.w3.org/2000/svg'>
-            <path fillRule='evenodd' clipRule='evenodd' d='M6 8C7.1 8 8 7.1 8 6C8 4.9 7.1 4 6 4C4.9 4 4 4.9 4 6C4 7.1 4.9 8 6 8ZM6 10C4.9 10 4 10.9 4 12C4 13.1 4.9 14 6 14C7.1 14 8 13.1 8 12C8 10.9 7.1 10 6 10ZM6 16C4.9 16 4 16.9 4 18C4 19.1 4.9 20 6 20C7.1 20 8 19.1 8 18C8 16.9 7.1 16 6 16Z' fill='white' />
-          </svg>
+            <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <path fillRule='evenodd' clipRule='evenodd' d='M9 1.875V1L6 1V1.875H3V15H12V1.875H9Z' fill='white' />
+            </svg>
+            <p className='text-sm text-white font-semibold'>12:30</p>
+          </div>
+          <div className='w-full flex items-center bg-whatsapp-600 pt-2 pb-3 px-2'>
+            <HeaderWhatsApp />
+            <svg width='12' height='24' viewBox='0 0 12 24' fill='none' className='ml-auto mr-2' xmlns='http://www.w3.org/2000/svg'>
+              <path fillRule='evenodd' clipRule='evenodd' d='M6 8C7.1 8 8 7.1 8 6C8 4.9 7.1 4 6 4C4.9 4 4 4.9 4 6C4 7.1 4.9 8 6 8ZM6 10C4.9 10 4 10.9 4 12C4 13.1 4.9 14 6 14C7.1 14 8 13.1 8 12C8 10.9 7.1 10 6 10ZM6 16C4.9 16 4 16.9 4 18C4 19.1 4.9 20 6 20C7.1 20 8 19.1 8 18C8 16.9 7.1 16 6 16Z' fill='white' />
+            </svg>
+          </div>
         </div>
         <div className='h-full overflow-y-auto relative max-h-[780px]'>
           <div className='flex w-full'>
